54f9f17d9980a862022890a71dac48054e250cd3
Ali Butt <alibutt152209@gmail.com>
2022-01-24 11:32:49 +0500

    Initial Commit

---
+++ War-Challenge/War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Ali Butt on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
